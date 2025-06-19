9dcbb396b6947ac222b6142425db0e31ac32ef3e
subarnosingh <subarnopro01@gmail.com>
2025-06-19 14:55:30 +0530

    Update ques9.c

--- Assignment 6/ques9.c
+++ Assignment 6/ques9.c
@@ -1,38 +1,26 @@
 #include <stdio.h>
-#include <string.h>
-
-#define MAX_STUDENTS 3
 
 struct Student {
     char name[50];
-    int rollNo;
-    float marks;
+    int marks;
 };
 
 int main() {
-    struct Student students[MAX_STUDENTS];
-    int i, topIndex = 0;
+    struct Student s[3];
+    int i, maxIndex = 0;
 
-    printf("Enter details for %d students:\n", MAX_STUDENTS);
-    for (i = 0; i < MAX_STUDENTS; i++) {
-        printf("\nStudent %d\n", i + 1);
-        printf("Enter name: ");
-        scanf(" %[^\n]", students[i].name); // Read string with spaces
-        printf("Enter roll number: ");
-        scanf("%d", &students[i].rollNo);
-        printf("Enter marks: ");
-        scanf("%f", &students[i].marks);
-
-        // Track index of highest scoring student
-        if (students[i].marks > students[topIndex].marks) {
-            topIndex = i;
-        }
+    for(i = 0; i < 3; i++) {
+        printf("Enter name and marks of student %d: ", i + 1);
+        scanf("%s %d", s[i].name, &s[i].marks);
     }
 
-    printf("\nStudent with the highest marks:\n");
-    printf("Name: %s\n", students[topIndex].name);
-    printf("Roll Number: %d\n", students[topIndex].rollNo);
-    printf("Marks: %.2f\n", students[topIndex].marks);
+    for(i = 1; i < 3; i++) {
+        if(s[i].marks > s[maxIndex].marks)
+            maxIndex = i;
+    }
+
+    printf("\nStudent with highest marks:\n");
+    printf("Name: %s, Marks: %d\n", s[maxIndex].name, s[maxIndex].marks);
 
     return 0;
 }
